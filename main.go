146650f50b8f6beaff418d02f780f107680f0df0
Dalei Li <dalei@messagebird.com>
2019-04-26 14:13:27 +0200

    subcommands: split and merge

--- main.go
+++ main.go
@@ -1,39 +1,21 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"os"
 
-	"github.com/sirupsen/logrus"
-	// "github.com/google/subcommands"
-)
-
-var (
-	fileName       = flag.String("file", "", "The big file to split")
-	size           = flag.Int("size", 20, "The maximal size per part in megabytes")
-	outputFileName = flag.String("output", "", "The prefix of output files, the same to file by default")
+	"github.com/google/subcommands"
 )
 
 func main() {
+	// Register subcommands, split and merge
+	subcommands.Register(subcommands.HelpCommand(), "")
+	subcommands.Register(&splitCmd{}, "")
+	subcommands.Register(&mergeCmd{}, "")
+
 	flag.Parse()
-
-	if *outputFileName == "" {
-		*outputFileName = *fileName
-	}
-
-	// split file
-	parts, err := Split(*fileName, *size*1024*1024, *outputFileName)
-	if err != nil {
-		logrus.Errorf("fail to split file %s, error: %v", *fileName, err)
-	}
-
-	logrus.Infof("write to parts: %v", parts)
-
-	// merge files
-	o := *outputFileName + ".new"
-	err = Merge(parts, o)
-	if err != nil {
-		logrus.Errorf("fail to merge files %v, error: %v", parts, err)
-	}
-	logrus.Infof("merge parts %v into file %s", parts, o)
+	ctx := context.Background()
+	os.Exit(int(subcommands.Execute(ctx)))
 }
 
